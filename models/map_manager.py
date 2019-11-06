c26dcf11e58fe5c6946cd23da8fa6d27ec31d2e2
Michael Dobler <michael.dobler@outlook.com>
2019-11-06 22:44:57 +0100

    Small tweaks

--- models/map_manager.py
+++ models/map_manager.py
@@ -28,10 +28,10 @@ from models.rectangle_obstacle import *
 OBS_MIN_DIM = 0.05  # meters
 OBS_MAX_DIM = 0.1  # meters
 OBS_MAX_COMBINED_DIM = 2.0  # meters
-OBS_MIN_COUNT = 30
-OBS_MAX_COUNT = 60
-OBS_MIN_DIST = 0.2  # meters
-OBS_MAX_DIST = 1  # meters
+OBS_MIN_COUNT = 5
+OBS_MAX_COUNT = 20
+OBS_MIN_DIST = 0.4  # meters
+OBS_MAX_DIST = 2  # meters
 GOAL_MIN_DIST = 1.0  # meters
 GOAL_MAX_DIST = 2.5  # meters
 MIN_GOAL_CLEARANCE = 0.5  # meters
