bcd3dfd980393d0d48f56c722ede69ebea3a568d
Michael Dobler <michael.dobler@outlook.com>
2019-12-20 13:23:19 +0100

    Remove unused function

--- utils/math_util.py
+++ utils/math_util.py
@@ -23,11 +23,3 @@ from math import *
 # map the given angle to the equivalent angle in [ -pi, pi ]
 def normalize_angle(theta):
     return atan2(sin(theta), cos(theta))
-
-
-def frange(x, y, jump=1.0):
-    r = []
-    while x < y:
-        r.append(x)
-        x += jump
-    return r
