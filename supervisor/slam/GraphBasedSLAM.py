c2d4e54e4283c3831ae07f18bd65a13fdc688724
yixing.lin <yixing.lin@tuhh.de>
2020-09-06 22:20:55 +0200

    Read values from wheel encoders

--- supervisor/slam/GraphBasedSLAM.py
+++ supervisor/slam/GraphBasedSLAM.py
@@ -1,34 +1,10 @@
 from supervisor.slam.graph.landmark_graph import *
-# class EKFSlam(Slam):
-#
-#     def __init__(self, supervisor_interface, slam_cfg, step_time):
-#         """
-#         Initializes an object of the EKFSlam class
-#         :param supervisor_interface: The interface to interact with the robot supervisor
-#         :param slam_cfg: The configuration for the SLAM algorithm
-#         :param step_time: The discrete time that a single simulation cycle increments
-#         """
-#         # Bind the supervisor interface
-#         self.supervisor = supervisor_interface
-#         # Extract relevant configurations
-#         self.dt = step_time
-#         self.distance_threshold = slam_cfg["ekf_slam"]["distance_threshold"]
-#         self.robot_state_size = slam_cfg["robot_state_size"]
-#         self.landmark_state_size = slam_cfg["landmark_state_size"]
-#         self.sensor_noise = np.diag([slam_cfg["sensor_noise"]["detected_distance"],
-#                                      np.deg2rad(slam_cfg["sensor_noise"]["detected_angle"])]) ** 2
-#         self.motion_noise = np.diag([slam_cfg["ekf_slam"]["motion_noise"]["x"],
-#                                      slam_cfg["ekf_slam"]["motion_noise"]["y"],
-#                                      np.deg2rad(slam_cfg["ekf_slam"]["motion_noise"]["theta"])]) ** 2
-#         # The estimated combined state vector, initially containing the robot pose at the origin and no landmarks
-#         self.mu = np.zeros((self.robot_state_size, 1))
-#         # The state covariance, initially set to absolute certainty of the initial robot pose
-#         self.Sigma = np.zeros((self.robot_state_size, self.robot_state_size))
 from supervisor.slam.Slam import Slam
 from utils.math_util import normalize_angle
 import numpy as np
 from math import *
 from models.Pose import Pose
+from time import sleep
 
 class GraphBasedSLAM(Slam):
 
@@ -53,9 +29,12 @@ class GraphBasedSLAM(Slam):
         #                              slam_cfg["graph_based_slam"]["motion_noise"]["y"],
         #                              np.deg2rad(slam_cfg["graph_based_slam"]["motion_noise"]["theta"])]) ** 2
         # self.min_distance_threshold = slam_cfg["graph_based_slam"]["distance_threshold"]
+        """              DEBUG             """
         self.sensor_noise = np.diag([0.1, 0.1])**2
         self.motion_noise = np.diag([0.1, 0.1, np.deg2rad(30)]) ** 2
         self.min_distance_threshold = 0.1
+        """                                 """
+
         # The estimated combined state vector, initially containing the robot pose at the origin and no landmarks
         self.mu = np.zeros((self.robot_state_size, 1))
         # The state covariance, initially set to absolute certainty of the initial robot pose
@@ -64,17 +43,17 @@ class GraphBasedSLAM(Slam):
 
         self.max_range = self.supervisor.proximity_sensor_max_range()
         self.min_range = self.supervisor.proximity_sensor_min_range()
-        self.robot_width = self.supervisor.wheel_base_length()
+        # self.robot_width = self.supervisor.wheel_base_length()
         # print (self.max_range, self.min_range)
 
         self.graph = LMGraph()
         self.old_wheel_record = (0, 0)
-        self.step_counter = 0
+        self.odom_pose = self.__reset_odometry_measurement() # accumulative odometry estimation from wheel encoders
 
+        self.step_counter = 0
         self.fix_hessian = 0
         self.__init_first_step()
 
-        self.old_wheel_record = (0, 0)
 
     def __init_first_step(self):
         """    add first pose-vertex     """
@@ -99,40 +78,73 @@ class GraphBasedSLAM(Slam):
         Returns the estimated pose of the robot
         """
         return Pose(self.mu[0, 0], self.mu[1, 0], self.mu[2, 0])
+        #return Pose(self.odom_pose[0, 0], self.odom_pose[1, 0], self.odom_pose[2, 0])
 
     def get_landmarks(self):
         """
         Returns the estimated landmark positions
         """
-        # lm = [(1,2), (3,3), (1,3)]
-        # return [(x, y) for (x, y) in lm]
         landmarks = []
         vertices = self.graph.get_estimated_landmark_vertices()
         for v in vertices:
             landmarks.append((v.pose[0], v.pose[1]))
         return landmarks
 
+    def __update_odometry_estimation(self, odom_pose):
+        """
+        Calculate odometry estimation from wheel encoders.
+        """
+        ticks, params = self.supervisor.read_wheel_encoders()  #  (ticks_left, ticks_right)
+        base_length = params['wheel_base_length']
+        R = params['wheel_radius']
+        N = params['wheel_encoder_ticks_per_revolution']
+        left_wheel_movement = 2 * np.pi * R * ticks[0] / N
+        right_wheel_movement = 2 * np.pi * R * ticks[1] / N
+        wheel_record = (left_wheel_movement, right_wheel_movement)
+        vl = (wheel_record[0] - self.old_wheel_record[0])/self.dt  # Velocity of the left wheel
+        vr = (wheel_record[1] - self.old_wheel_record[1])/self.dt  # Velocity of the right wheel
+        v, w = self.diff_to_uni(vl, vr, base_length)   # Convert to uni-wheel model
+        #print ("v, w", v, w)
+        # Update the odometry estimation
+        odom_pose = self.motion_model(odom_pose, np.array([[v], [w]]), self.dt)
+        self.old_wheel_record = np.copy(wheel_record)
+        return odom_pose
+
+    def __reset_odometry_measurement(self):
+        return np.copy(self.mu)
+
     def update(self, u, z):
         """
         Executes an update cycle of the SLAM algorithm
-        :param u: motion command, A single item is a vector of [[velocity], [angular_velocity]]
+        :param u: Motion command, A single item is a vector of [[translational_velocity], [angular_velocity]]
         :param z: List of measurements. A single item is a tuple of (range, angle)
         """
-        self.mu = self.motion_model(self.mu, u, self.dt)
-        J = self.jaco_motion_model(self.mu, u, self.dt)  # calculate jacobian matrix
+
+        self.mu = self.motion_model(self.mu, u, self.dt) # Calculate next step
+        J = self.jaco_motion_model(self.mu, u, self.dt)  # Calculate jacobian matrix
         # print ("helo", self.Sigma.shape, J.shape, self.motion_noise.shape)
-        self.Sigma = J.T @ self.Sigma @ J + self.motion_noise  # update covariance matrix
+        self.Sigma = J.T @ self.Sigma @ J + self.motion_noise  # Update covariance matrix
+        self.odom_pose = self.__update_odometry_estimation(self.odom_pose) # Update accumulative odometry estimation
         self.step_counter += 1
 
+        """        Update the Graph         """
         if self.step_counter >  5: # create a vertex each n steps
             print('[COUNT] POSEES, LANDMARKS :', self.graph.count_vertices())
             self.__front_end(z)
             self.step_counter = 0
-
+            self.odom_pose = self.__reset_odometry_measurement() # reset odom_pose
             num_poses, _ = self.graph.count_vertices()
+
             if num_poses % 5 == 0 and num_poses > 0:
-                # self.__back_end()
-                self.graph.draw()
+                self.__back_end() # graph optimization
+                last_vertex = self.graph.get_last_pose_vertex()
+                self.mu = np.copy(last_vertex.pose)   # update current state
+                self.Sigma = np.copy(self.motion_noise)
+                self.odom_pose = self.__reset_odometry_measurement()  # Reset odometry estimation
+
+
+            self.graph.draw()
+
 
     def __front_end(self, z):
         """
@@ -167,40 +179,21 @@ class GraphBasedSLAM(Slam):
         #     self.graph.add_edge(vertex2, vertex3, meas, info)
 
         """      calculate pose-pose edge       """
-        # wheel_record = self.interface.read_wheel_encoder()
-        wheel_record = self.supervisor.read_wheel_encoders()
-        vl = (wheel_record[0] - self.old_wheel_record[0])/self.dt
-        vr = (wheel_record[1] - self.old_wheel_record[1])/self.dt
-        v, w = self.diff_to_uni(vl, vr, self.robot_width)
-
-        # calculate actual measurement and information matrix
-        #meas, info = self.__convert_pose_pose_raw_measurement([v, w])
-        #odom_pose = self.supervisor.estimated_pose()  # The supervisors internal pose estimation based on odometry
-        #odom_pose = np.array(odom_pose.sunpack()).reshape((self.robot_state_size, 1))
-
-        """debug"""
-        # odom_vertex = PoseVertex(odom_pose, self.Sigma)
-        # self.graph.add_vertex(odom_vertex)
-        # meas, info = self.__convert_pose_pose_raw_measurement(vertex1.pose, odom_pose)
-        # self.graph.add_edge(vertex1, odom_pose, meas, info)
-        """!!!!"""
-        #print (odom_pose, vertex2)
-        meas, info = self.__convert_pose_pose_raw_measurement([v, w])
-        print ("pp_constraint1", meas.T)
-        print("pp_constraint2", vertex1.pose.T, vertex2.pose.T)
+        meas, info = self.__convert_pose_pose_raw_measurement(vertex1.pose, self.odom_pose)
         self.graph.add_edge(vertex1, vertex2, meas, info)
-        print("pp_constraint2 error", self.graph.edges[-1].calc_error())
-        self.old_wheel_record = np.copy(wheel_record)
+
+        #
+        # plt.cla()
+        # plt.plot(vertex1.pose[0,0], vertex1.pose[1,0], 'k*')
+        # plt.plot(vertex2.pose[0,0], vertex2.pose[1,0], 'r*')
+        # plt.plot(self.odom_pose[0,0], self.odom_pose[1,0], 'gx')
+        # plt.show(block=True)
+
 
     def __back_end(self):
         print('SLAM OPTIMIZATION...')
         error = self.graph.graph_optimization(number_fix=self.fix_hessian, damp_factor=1)
         print('SLAM OPTIMIZATION: GLOBAL ERROR', error)
-        """     update current state    """
-        # last_vertex = self.__search_last_pose_vertex()
-        last_vertex = self.graph.get_last_pose_vertex()
-        self.mu = np.copy(last_vertex.pose)
-        self.Sigma = np.zeros((self.robot_state_size, self.robot_state_size))
 
     def __convert_pose_landmark_raw_measurement(self, zi):
         info = inv(self.sensor_noise)
@@ -208,20 +201,12 @@ class GraphBasedSLAM(Slam):
                             [zi[0] * sin(zi[1])]])
         return meas_xy, info
 
-    def __convert_pose_pose_raw_measurement(self, u):
+    def __convert_pose_pose_raw_measurement(self, pose, odom_pose):
         """
         Convert raw measurement u to the measurement defined by the error metric
-        :param vertex1: previous pose of the robot
-        :param vertex2:
-        :param u: measurement calculated from the wheel encoders, u = [[v], [w]]
-        :return:
-            meas: a vector of the transformation matrix from the previous pose to the current pose.
-            info:
         """
-        v, w = u
-        u_meas = np.array([[v], [w]])
-        pose1 = np.zeros((3,1))
-        pose2 = self.motion_model(pose1, u_meas, self.dt)  # expected pose of next step
+        pose1 = pose
+        pose2 = odom_pose  # expected pose of next step
         M = inv(v2t(pose1)) @ v2t(pose2)  # transformation matrix from x_meas to vertex1.pose
         info = inv(self.motion_noise)
         meas = t2v(M)
@@ -336,6 +321,12 @@ class GraphBasedSLAM(Slam):
 
     @staticmethod
     def diff_to_uni(v_l, v_r, width):
+        """
+        :param v_l: Translational velocity of the left wheel
+        :param v_r: Translational velocity of the right wheel
+        :return v, w
+                Translational and angular velocities
+        """
         v = (v_r + v_l) * 0.5
         w = (v_r - v_l) / width
         return v, w
