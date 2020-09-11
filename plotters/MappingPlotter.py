06f0eaf7ad50ceec62cb2e089e949dcefd59755f
yixing.lin <yixing.lin@tuhh.de>
2020-09-11 17:35:04 +0200

    Draw occupancy mapping on the frames

--- plotters/MappingPlotter.py
+++ plotters/MappingPlotter.py
@@ -1,11 +1,8 @@
-from models.obstacles.RectangleObstacle import RectangleObstacle
-from plotters.ObstaclePlotter import ObstaclePlotter
-from models.Pose import Pose
 class MappingPlotter:
 
     def __init__(self, slam_mapping, viewer, frame_number):
         """
-
+        Initialize a MappingPlotter object
         :param slam_mapping: An object of OccupancyGridMap2d
         :param viewer: The viewer to be used
         :param frame_number: The frame number to be used
@@ -13,23 +10,32 @@ class MappingPlotter:
         self.slam_mapping = slam_mapping
         self.viewer = viewer
         self.frame_number = frame_number
-
         self.resolution = slam_mapping.resolution # number of pixels per meter
         self.pixel_size = 1/self.resolution # pixel size in meters
         self.offset = slam_mapping.offset  # offset in meters
-        self.counter = 0
 
     def draw_mapping_to_frame(self):
+        """
+        Draw the occupancy gridmap estimated by the mapping algorithm to the frame
+        """
         frame = self.viewer.current_frames[self.frame_number]
         H, W = self.slam_mapping.map_shape()
+        map = self.slam_mapping.get_map()
         for j in range(H):
             for i in range(W):
-                val = self.slam_mapping.get_value(i, j)
-                if val > 0.5:
+                val = map[j, i]  # the occupancy probability
+                if val >= 0.5:
                     x, y = self.__to_meter(i, j)
-                    frame.add_circle([y, x], self.pixel_size/2, color='gray73', alpha=val)
+                    frame.add_rectangle([x, y], self.pixel_size, self.pixel_size, color=(val, val, val), alpha=0.5)
 
     def __to_meter(self, x, y):
+        """
+        Calculate the position of a point in pixels by its position in meters
+        :param x: x in pixels
+        :param y: y in pixels
+        :return:
+                x, y: the position of a point in meters
+        """
         x = x/self.resolution - self.offset[0]
         y = y/self.resolution - self.offset[1]
         return x, y
